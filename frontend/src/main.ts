0945fc3c1fd54f040a2ec1d793211e246142518e
jannik-reaZn <jannik.harm@gmail.com>
2025-01-18 23:10:57 +0100

    Added example env

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import axios from "axios";
-
-// axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 const app = createApp(App);
-// axios.defaults.baseURL = `${import.meta.env.VITE_AXIOS_BASEURL}${
-//   import.meta.env.VITE_AXIOS_BACKENDURL
-// }`;
-// axios.defaults.headers = {
-//   ...axios.defaults.headers,
-//   "Content-Type": "application/json",
-// };
 
 app.mount("#app");
