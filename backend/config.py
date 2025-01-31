8da058791c45616ff01ff19298957bf4e2060ce3
jannik-reaZn <jannik.harm@gmail.com>
2025-01-31 15:20:56 +0100

    Added config file with pydantic settings

---
+++ backend/config.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+from typing import Annotated
+
+from fastapi import Depends
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = "Afterwork Coding"
+    cors_origins: list = ["http://127.0.0.1:5173", "http://localhost:5173"]
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings = get_settings()
+settingsDep = Annotated[Settings, Depends(get_settings)]
--- backend/middlewares.py
+++ backend/middlewares.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from backend.config import settings
+
 
 def register_middlewares(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  # ["http://localhost:5173/"]
+        allow_origins=settings.cors_origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
