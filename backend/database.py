ee81f547934e21fc51b356bb3fc45879b639dff7
jannik-reaZn <jannik.harm@gmail.com>
2025-01-31 15:28:26 +0100

    Added types and dependency object for db session

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,22 @@
-from sqlmodel import create_engine, SQLModel, Session
+from typing import Annotated, Generator
+
+from fastapi import Depends
 from sqlalchemy.orm import sessionmaker
+from sqlmodel import Session, SQLModel, create_engine
 
 SQLITE_DATABASE_URL = "sqlite:///backend/development.db"
 
-engine = create_engine(
-    SQLITE_DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLITE_DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def create_db_and_tables():
+def create_db_and_tables() -> None:
     SQLModel.metadata.create_all(engine)
 
 
-def get_db():
+def get_db() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
+
+
+SessionDep = Annotated[Session, Depends(get_db)]
