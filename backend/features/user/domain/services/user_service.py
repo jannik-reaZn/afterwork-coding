4bd8f140b0a2e5a35b9ed3eaeae7797ce7a63ad0
jannik-reaZn <jannik.harm@gmail.com>
2025-03-01 11:22:14 +0100

    Adapted import route

--- backend/features/user/domain/services/user_service.py
+++ backend/features/user/domain/services/user_service.py
@@ -1,8 +1,8 @@
 from fastapi import Depends
 
+from backend.features.user.repositories import UserRepository
 from backend.features.user.repositories.sql.entities.user_entity import UserSqlEntity
 from backend.features.user.repositories.sql.user_repository_sql import UserRepositorySql
-from backend.features.user.repositories.user_repository import UserRepository
 
 
 class UserService:
