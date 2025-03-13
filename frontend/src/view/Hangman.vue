2ec626b845202ef51d62c7b41a746fda7e3f45ca
jannik-reaZn <jannik.harm@gmail.com>
2025-03-13 16:22:59 +0100

    Stat of hangman

---
+++ frontend/src/view/Hangman.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Hangman</h1>
+    <div class="grid gap-2">
+        <Button
+            v-for="letter in alphabet"
+            :key="letter"
+            :label="letter"
+            class="p-2"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const value = ref("");
+
+
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+</script>
+
+<style scoped>
+</style>
