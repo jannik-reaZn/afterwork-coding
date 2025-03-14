51332b99a4351f71d25b1b29ac731b8c2ffd6053
jannik-reaZn <jannik.harm@gmail.com>
2025-03-14 13:08:08 +0100

    Temp

--- frontend/src/view/Home.vue
+++ frontend/src/view/Home.vue
@@ -1,17 +1,50 @@
 <template>
-  <Card>
-    <template #title>Hangman</template>
+  <Card class="custom-card">
+    <template #title>
+      <h2 class="p-card-title text-center">Hangman</h2>
+    </template>
     <template #content>
-      <p class="m-0">Description for hangman game.</p>
+      <p class="p-card-content text-gray-600 text-center">
+        Try to guess the word before you run out of attempts!
+      </p>
     </template>
     <template #footer>
-      <div class="flex gap-4 mt-1">
-        <RouterLink to="/hangman">Open</RouterLink>
+      <div class="p-card-footer">
+        <Button
+          class="px-4 py-2 text-white rounded-lg shadow-md hover:bg-blue-600 transition duration-200"
+          @click="openGame"
+          >Open Game
+        </Button>
       </div>
     </template>
   </Card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from "vue-router";
 
-<style scoped></style>
+const router = useRouter();
+
+const openGame = () => {
+  router.push("/hangman");
+};
+</script>
+
+<style scoped>
+.custom-card {
+  --p-card-background: white;
+  --p-card-border-radius: 1rem;
+  --p-card-color: black;
+  border: 2px solid lightgrey;
+}
+
+.p-card-title {
+  --p-card-title-font-size: 1.5rem;
+  --p-card-title-font-weight: bold;
+}
+
+.p-card-footer {
+  display: flex;
+  justify-content: center;
+}
+</style>
