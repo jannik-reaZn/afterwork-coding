3c1e18171abc0627ee78443a17e9ae4c28939d87
Abilityz <mert.adler@me.com>
2025-03-23 16:10:34 +0100

    Extension and improvement of the update_game logic

--- backend/features/hangman/domain/services/hangman_service.py
+++ backend/features/hangman/domain/services/hangman_service.py
@@ -3,7 +3,7 @@ class Hangman:
     def __init__(self, total_tries: int = 6):
         self.total_tries: int = total_tries
         self.random_word: str = self.generate_random_word()
-        self.guessed_letters: list[str] = []
+        self.guessed_letters: set[str] = set()
 
     def return_values(self):
         return {
@@ -26,14 +26,16 @@ class Hangman:
     #   return False
 
     def update_game(self, guessed_letter: str):
-        if not self.is_game_finished():
-            self.guessed_letters.append(guessed_letter)
+
+        if self.is_game_finished():
+            return self.return_values()
+
+        if guessed_letter not in self.guessed_letters:
+            self.guessed_letters.add(guessed_letter)
             if guessed_letter not in self.random_word:
                 self.total_tries -= 1
 
-            return self.return_values()
-        else:
-            return self.return_values()
+        return self.return_values()
 
 
 hangman = Hangman()
@@ -43,4 +45,13 @@ print("Initiate Game: ", hangman_status)
 print()
 
 hangman_status = hangman.update_game(guessed_letter="a")
+print("Update Game: ", hangman_status)
+
+hangman_status = hangman.update_game(guessed_letter="a")
+print("Update Game: ", hangman_status)
+
+hangman_status = hangman.update_game(guessed_letter="b")
+print("Update Game: ", hangman_status)
+
+hangman_status = hangman.update_game(guessed_letter="h")
 print("Update Game: ", hangman_status)
