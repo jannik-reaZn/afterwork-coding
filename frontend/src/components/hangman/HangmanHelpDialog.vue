a51ba293754cdd7476db69844729493379d3e08d
jannik-reaZn <jannik.harm@gmail.com>
2025-03-30 16:30:35 +0200

    fixed modal not beeing draggable

--- frontend/src/components/hangman/HangmanHelpDialog.vue
+++ frontend/src/components/hangman/HangmanHelpDialog.vue
@@ -1,5 +1,10 @@
 <template>
-  <Dialog v-model:visible="showDialog" modal header="Game Rules">
+  <Dialog
+    v-model:visible="showDialog"
+    modal
+    header="Game Rules"
+    :draggable="false"
+  >
     <ul class="list-disc pl-5">
       <li v-for="(rule, idx) in rules" :key="idx">{{ rule }}</li>
     </ul>
@@ -13,6 +18,5 @@ const rules = [
   "Guess the word by suggesting letters.",
   "Only one character per guess.",
   "If you guess the word before running out of tries, you win!",
-  "You can guess incorrectly 6 times before losing.",
 ];
 </script>
