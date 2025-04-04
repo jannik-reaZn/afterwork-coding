54447adbcfb6423636a4dc553266b8e402a15104
jannik-reaZn <jannik.harm@gmail.com>
2025-04-04 23:09:39 +0200

    Added comments

--- backend/common/domain/models/domain_model.py
+++ backend/common/domain/models/domain_model.py
@@ -3,4 +3,8 @@ from pydantic.alias_generators import to_camel
 
 
 class DomainModel(BaseModel):
+    """
+    This class provides a common configuration for all domain models in the application.
+    """
+
     model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
--- backend/features/hangman/routes/requests/hangman_requests.py
+++ backend/features/hangman/routes/requests/hangman_requests.py
@@ -1,10 +1,19 @@
 from typing import List
 
+from pydantic import Field
+
 from backend.common.domain.models import DomainModel
 
 
 class HangmanRequest(DomainModel):
-    random_word: str
-    total_tries: int
-    guessed_letters: List[str]
-    is_game_won_status: bool
+    random_word: str = Field(description="The random word for the hangman game.")
+    total_tries: int = Field(
+        ge=0,
+        description="The total tries for the hangman game.",
+    )
+    guessed_letters: List[str] = Field(
+        description="The letters guessed by the user.",
+    )
+    is_game_won_status: bool = Field(
+        description="Indicates whether a game is won.",
+    )
