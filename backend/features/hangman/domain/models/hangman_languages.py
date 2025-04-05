8a6b8fbb644fbb7fa743ddfa2ed5824d5d18edf4
jannik-reaZn <jannik.harm@gmail.com>
2025-04-05 18:19:06 +0200

    Added comments

--- backend/features/hangman/domain/models/hangman_languages.py
+++ backend/features/hangman/domain/models/hangman_languages.py
@@ -5,17 +5,32 @@ from backend.common.domain.models import DomainModel
 
 
 class HangmanLanguage(StrEnum):
+    """
+    Enum representing the available languages for the Hangman game.
+
+    Attributes:
+        AMERICAN: Represents the American English language.
+        GERMAN: Represents the German language.
+    """
+
     AMERICAN = auto()
     GERMAN = auto()
 
 
-# Language-specific alphabets
+# Mapping between each HangmanLanguage to its corresponding list of alphabet characters.
 LANGUAGE_ALPHABETS: Dict[HangmanLanguage, List[str]] = {
     HangmanLanguage.AMERICAN: list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
     HangmanLanguage.GERMAN: list("ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜß"),
 }
 
 
-# Pydantic model for response
 class HangmanAlphabet(DomainModel):
+    """
+    Represents the alphabet used in the Hangman game.
+
+    Attributes:
+        alphabet (List[str]): A list of characters representing the alphabet available for the
+        Hangman game.
+    """
+
     alphabet: List[str]
