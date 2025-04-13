de1837a4790f23db73dbfd82c6e28c214e7124d5
Jannik Harm <64127899+jannik-reaZn@users.noreply.github.com>
2025-04-13 21:26:05 +0200

    Added port to vite config (#54)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import VueRouter from "unplugin-vue-router/vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
@@ -6,23 +6,28 @@ import tailwindcss from "@tailwindcss/vite";
 import Components from "unplugin-vue-components/vite";
 import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 
-export default defineConfig({
-  plugins: [
-    VueRouter(),
-    vue(),
-    tailwindcss(),
-    Components({
-      resolvers: [PrimeVueResolver()],
-    }),
-  ],
-  server: {
-    watch: {
-      usePolling: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      VueRouter(),
+      vue(),
+      tailwindcss(),
+      Components({
+        resolvers: [PrimeVueResolver()],
+      }),
+    ],
+    server: {
+      port: parseInt(env.VITE_PORT, 10) || 3000,
+      watch: {
+        usePolling: true,
+      },
     },
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
     },
-  },
+  };
 });
