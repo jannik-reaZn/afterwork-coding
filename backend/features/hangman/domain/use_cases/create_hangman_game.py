7f4e69f90da4496c1618bb344a2ab745485f5cb3
Jannik Harm <64127899+jannik-reaZn@users.noreply.github.com>
2025-04-13 12:15:04 +0200

    Replaced miniconda with miniforge (#36)
    Replaced miniconda with miniforge
    Added docstrings

--- backend/features/hangman/domain/use_cases/create_hangman_game.py
+++ backend/features/hangman/domain/use_cases/create_hangman_game.py
@@ -6,9 +6,28 @@ from backend.features.hangman.domain.word_provider.interface import WordProvider
 
 class StartHangmanGameUseCase:
     def __init__(self, word_provider_factory: Callable[[str], WordProviderInterface]):
+        """
+        Initializes the CreateHangmanGame use case.
+
+        Args:
+            word_provider_factory: A factory function that takes a string and returns an instance
+            of WordProviderInterface, used to provide words for the hangman game.
+        """
         self.word_provider_factory = word_provider_factory
 
     def __call__(self, total_tries: int, language: str) -> HangmanGame:
+        """
+        Creates a new HangmanGame instance with a random word and specified total tries.
+
+        Args:
+            total_tries (int): The total number of tries allowed for the game.
+            language (str): The language to be used for selecting the random word.
+
+        Returns:
+            HangmanGame: A new instance of the HangmanGame initialized with a random word,
+            the specified total tries, an empty list of guessed letters, and a default
+            game-won status of False.
+        """
         word_provider = self.word_provider_factory(language)
         random_word = word_provider.get_random_word()
         return HangmanGame(
--- backend/features/hangman/domain/use_cases/update_hangman_game.py
+++ backend/features/hangman/domain/use_cases/update_hangman_game.py
@@ -3,6 +3,16 @@ from backend.features.hangman.domain.models import HangmanGame
 
 class GuessHangmanLetterUseCase:
     def __call__(self, hangman_status: HangmanGame, guessed_letter: str) -> HangmanGame:
+        """
+        Updates the state of the Hangman game based on the guessed letter.
+
+        Args:
+            hangman_status (HangmanGame): The current state of the Hangman game.
+            guessed_letter (str): The letter guessed by the player.
+
+        Returns:
+            HangmanGame: The updated state of the Hangman game.
+        """
         if self._is_game_lost(hangman_status):
             return hangman_status
 
@@ -17,6 +27,16 @@ class GuessHangmanLetterUseCase:
         return hangman_status
 
     def _is_game_won(self, hangman_status: HangmanGame) -> bool:
+        """
+        Determines if the Hangman game has been won.
+
+        Args:
+            hangman_status (HangmanGame): The current status of the Hangman game,
+                including the random word to guess and the letters guessed so far.
+
+        Returns:
+            bool: True if all letters in the random word have been guessed, False otherwise.
+        """
         return (
             True
             if set(hangman_status.random_word).issubset(set(hangman_status.guessed_letters))
@@ -24,4 +44,14 @@ class GuessHangmanLetterUseCase:
         )
 
     def _is_game_lost(self, hangman_status: HangmanGame) -> bool:
+        """
+        Determines if the hangman game is lost.
+
+        Args:
+            hangman_status (HangmanGame): The current status of the hangman game,
+                including the number of remaining tries.
+
+        Returns:
+            bool: True if the game is lost (i.e., no remaining tries), False otherwise.
+        """
         return True if hangman_status.total_tries == 0 else False
