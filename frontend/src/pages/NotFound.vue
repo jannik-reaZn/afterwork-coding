e7709404a21d701bf36343eb5b644ab1aa9f54c4
jannik-reaZn <jannik.harm@gmail.com>
2025-04-14 20:55:06 +0200

    Added NotFound route

---
+++ frontend/src/pages/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-1 flex-col items-center justify-center">
+    <h1 class="text-4xl font-bold">404 - Page Not Found</h1>
+    <p class="mt-4">The page you are looking for does not exist.</p>
+    <router-link to="/" class="mt-4 text-blue-500 hover:underline">
+      Go back to Home
+    </router-link>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
